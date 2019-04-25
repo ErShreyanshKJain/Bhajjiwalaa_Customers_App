f119113c97dc07852ba50d1daa274df83a021cf5
SAP\sk <singh.shailesh37@gmail.com>
2019-04-25 15:53:07 +0530

    Progress dialog added

--- app/src/main/java/com/shreyanshjain/bhajjiwalaa_customers_app/login/LoginActivity.java
+++ app/src/main/java/com/shreyanshjain/bhajjiwalaa_customers_app/login/LoginActivity.java
@@ -2,6 +2,7 @@ package com.shreyanshjain.bhajjiwalaa_customers_app.login;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputEditText;
@@ -44,11 +45,20 @@ public class LoginActivity extends AppCompatActivity {
     CardView numCard,verifyCard;
     PhoneVerification phone;
     String mVerificationId;
+    AlertDialog.Builder builder;
+    AlertDialog dialog;
+    private static int LOGTIME_OUT=1500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        builder = new AlertDialog.Builder(LoginActivity.this);
+        builder.setCancelable(false); // if you want user to wait for some process to finish,
+        builder.setView(R.layout.dialog_progress);
+        dialog = builder.create();
+
 
         app_logo = findViewById(R.id.login_logo);
         login_layout =  findViewById(R.id.login_layout);
@@ -82,6 +92,7 @@ public class LoginActivity extends AppCompatActivity {
                     return;
                 }
 
+                dialog.show();
                 sendVerificationCode(num);
 //              openDialogBox();
 
@@ -110,6 +121,7 @@ public class LoginActivity extends AppCompatActivity {
                             verifyCode.requestFocus();
                             return;
                         }
+                        dialog.show();
                         verifyVerificationCode(code);
                     }
                 });
@@ -119,6 +131,7 @@ public class LoginActivity extends AppCompatActivity {
 
     public void sendVerificationCode(String mobile)
     {
+
         Log.d("Method","Send Verification Code");
         PhoneAuthProvider.getInstance().verifyPhoneNumber(
                 "+91" + mobile,
@@ -126,6 +139,7 @@ public class LoginActivity extends AppCompatActivity {
                 TimeUnit.SECONDS,
                 TaskExecutors.MAIN_THREAD,
                 mCallbacks);
+        dialog.dismiss();
     }
 
     private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
@@ -158,6 +172,7 @@ public class LoginActivity extends AppCompatActivity {
         PhoneAuthCredential credential = PhoneAuthProvider.getCredential(mVerificationId,code);
 
         Log.d("Method","verify verification :" + code);
+        dialog.show();
         signInWithPhoneAuthCredential(credential);
     }
 
@@ -167,15 +182,26 @@ public class LoginActivity extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()){
-                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                            startActivity(intent);
+//                            dialog.show();
+                            new Handler().postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                    startActivity(intent);
+                                    dialog.dismiss();
+                                    // close this activity
+                                    finish();
+                                }
+                            }, LOGTIME_OUT);
                         }
                         else{
+                            dialog.dismiss();
                             String message = "Somthing is wrong, we will fix it soon...";
 
                             if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
                                 message = "Invalid code entered...";
+
                             }
 
                             Snackbar snackbar = Snackbar.make(findViewById(R.id.parent), message, Snackbar.LENGTH_LONG);
